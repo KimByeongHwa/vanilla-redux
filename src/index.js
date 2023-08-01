f6228590dbf51508f76888a5ed791f2fe6e0cb44
KimByeongHwa <iambyeonghwa@gmail.com>
2023-08-01 15:54:02 +0900

    Vanilla JS Counter

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { legacy_createStore as createStore } from 'redux';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const add = document.getElementById('add');
+const minus = document.getElementById('minus');
+const number = document.querySelector('span');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+number.innerText = 0;
+
+const ADD = 'ADD';
+const MINUS = 'MINUS';
+
+// 리듀서는 state와 action을 인자로 받는다.(EventListener 같은 역할)
+const countReducer = (count = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
+};
+
+const countStore = createStore(countReducer);
+
+const onChage = () => {
+  number.innerText = countStore.getState();
+};
+
+// subscribe을 통해 html을 update 해준다.
+countStore.subscribe(onChage);
+
+// dispatch를 통해 액션을 발생시킨다.(triggering an event)
+// dispatch의 인자로 action을 넣는다.
+// action은 항상 Object 타입이어야 한다.
+const handleAdd = () => {
+  countStore.dispatch({ type: ADD });
+};
+
+const handleMinus = () => {
+  countStore.dispatch({ type: MINUS });
+};
+
+add.addEventListener('click', handleAdd);
+minus.addEventListener('click', handleMinus);
